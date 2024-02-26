573930b527e1d9469beee2e01f71ef845a05ced7
Alex Eftimie <65734304+alex-eftimie@users.noreply.github.com>
2024-02-26 15:09:09 +0200

    changed name

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,8 @@ let mix = require('laravel-mix')
 require('./nova.mix')
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/card.js', 'js')
-  .vue({ version: 3 })
-  .css('resources/css/card.css', 'css')
-  .nova('ideatocode/nova-spacer-card')
+    .setPublicPath('dist')
+    .js('resources/js/card.js', 'js')
+    .vue({ version: 3 })
+    .css('resources/css/card.css', 'css')
+    .nova('helios-live/nova-spacer-card')
